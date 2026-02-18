761e4cd94cf9b033342c0307fbc042f6d1ddc0ab
AItern <aitern@Interns-Mac-mini.local>
2026-02-18 16:40:56 +0100

    Initial build: random-showcase

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename="/random-showcase/">
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </ThemeProvider>
